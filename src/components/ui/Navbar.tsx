831d466fa381f07da67284f82c82dd32195b4415
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-06-30 15:35:21 +0200

    feat: add Navbar component

---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+       <div className="navbar navbar-dark bg-dark mb-4">
+           <span className="navbar-brand">
+               Peter
+           </span>
+
+           <button className="btn btn-outline-danger">
+               <span>Exit</span>
+           </button>
+       </div>
+    )
+}
+export default Navbar;
