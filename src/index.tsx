aff20cf8c032921bd4eb3be6e7d8720907dbe4ba
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-06-30 15:33:54 +0200

    cfg: initial cofiguration

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import CalendarApp from './CalendarApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CalendarApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
