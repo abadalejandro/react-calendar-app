2c1bc6cb8d7004536b5a8c52002a6af5482e2fda
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-07-06 16:50:24 +0200

    feat: add fetchWithToken method

--- src/helpers/fetch.tsx
+++ src/helpers/fetch.tsx
@@ -28,3 +28,28 @@ export const fetchWithoutToken = (endpoint: string, data: {}, method: httpMethod
     }
 
 }
+
+export const fetchWithToken = (endpoint: string, data?: {}, method: httpMethod = httpMethod.GET) => {
+    const url = `${baseUrl}/${endpoint}`;
+    const token = localStorage.getItem('token') || '';
+
+
+    if (method === httpMethod.GET) {
+        return fetch(url, {
+            method,
+            headers: {
+                'x-token': token
+            }
+        });
+    } else {
+        return fetch(url, {
+            method,
+            headers: {
+                'Content-type': 'application/json',
+                'x-token': token,
+            },
+            body: JSON.stringify(data)
+        });
+    }
+
+}
