4470f5994cd42380447cd6ae9dba05e54a89b7ca
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-07-06 18:28:39 +0200

    feat: add current name to navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,15 +1,20 @@
-const Navbar = () => {
-    return (
-       <div className="navbar navbar-dark bg-dark mb-4">
-           <span className="navbar-brand">
-               Peter
-           </span>
+import { useSelector } from 'react-redux';
+import { IRootReducer } from '../../redux/store';
 
-           <button className="btn btn-outline-danger">
-               <i className="fas fa-sign-out-alt"></i>
-               <span> Exit</span>
-           </button>
-       </div>
+const Navbar = () => {
+    const { name } = useSelector((state: IRootReducer) => state.auth);
+
+    return (
+        <div className="navbar navbar-dark bg-dark mb-4">
+            <span className="navbar-brand">
+                {name}
+            </span>
+
+            <button className="btn btn-outline-danger">
+                <i className="fas fa-sign-out-alt"></i>
+                <span> Exit</span>
+            </button>
+        </div>
     )
 }
 export default Navbar;
